4bbb0dcf8f5b1d2d9293627a5906ec6e61a1c410
ssoni9 <74078539+ssoni9@users.noreply.github.com>
2021-12-07 15:18:20 -0800

    Delete INS_monitor.m

--- GTM/proc/INS_monitor.m
+++
@@ -1,8 +0,0 @@
-function f_curr = INS_monitor(prob, prob_tight)
-    q = norm(prob_tight.res);
-    if q > 50
-        f_curr = prob_tight.f_vec;
-    else
-        f_curr = zeros(length(prob.rho), 1);
-    end
-end
